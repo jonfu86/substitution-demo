71a0dfe2739c2e971b49fa27d87faabb7216228c
jonfu86 <noreply>
2014-08-25 18:08:17 +0000

    Add component static

---
+++ ui/static.reel/static.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/static.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Static
+ * @extends Component
+ */
+exports.Static = Component.specialize(/** @lends Static# */ {
+    constructor: {
+        value: function Static() {
+            this.super();
+        }
+    }
+});
